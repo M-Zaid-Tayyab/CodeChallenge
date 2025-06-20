b5804c8309547a05cca5ded64fd780f3c7da935d
Muhammad Zaid <zaidtayyab1@gmail.com>
2025-06-20 17:05:03 +0500

    Session persisted

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,30 @@
-import { Slot } from "expo-router";
+import { Slot, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
 import { Platform } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
+import { useAuthStore } from "./auth/_store/authStore";
 
 export default function RootLayout() {
+  const { session, loading, init } = useAuthStore();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    init();
+  }, [init]);
+
+  useEffect(() => {
+    if (loading) return;
+
+    const inAuthGroup = segments[0] === "auth";
+
+    if (session && inAuthGroup) {
+      router.replace("/journal/list");
+    } else if (!session && !inAuthGroup) {
+      router.replace("/auth/signin");
+    }
+  }, [session, loading, segments, router]);
   return (
     <SafeAreaProvider>
       <Slot />
